5b7e434e5abd1a54c9ee04c15c73b6d4576c3d92
Halimp <halimputra0701@gmail.com>
2023-12-31 23:57:55 +0700

    feat: add distucturing detail post

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,28 +1,46 @@
-import Layout from '@components/Layout';
-import Container from '@components/Container';
-import PostMetaTitle from '@components/PostMetaTitle';
-import PostAuthor from '@components/PostAuthor';
-import Head from 'next/head';
+import Layout from "@components/Layout";
+import Container from "@components/Container";
+import PostMetaTitle from "@components/PostMetaTitle";
+import PostAuthor from "@components/PostAuthor";
+import Head from "next/head";
+import { formatDate } from "utils/utils";
+
+// ADDING REACTMARKDOWN
+import ReactMarkdown from "react-markdown";
 
 // ADDING FUNCTION FETCHING
 export async function getServerSideProps({ params: { slug } }) {
-  const reqDetail = await fetch(process.env.NEXT_PUBLIC_APIURL + '/posts?slug=' + slug);
+  const reqDetail = await fetch(
+    process.env.NEXT_PUBLIC_APIURL + "/posts?slug=" + slug
+  );
   const single = await reqDetail.json();
 
   // ADDING LOGIC FOR NOT FOUND PAGE
-  if (!single.length) 
-  return {
-    notFound: true
-  }
+  if (!single.length)
+    return {
+      notFound: true,
+    };
 
   return {
     props: {
-      single: single.length > 0 ? single[0] : {}
-    }
-  }
+      single: single.length > 0 ? single[0] : {},
+    },
+  };
 }
 
-export default function Detail({ single }) {
+export default function Detail({
+  // ADDING DISTRUCTURING DETAIL POST
+  single: {
+    title,
+    slug,
+    category,
+    published_at,
+    author,
+    thumbnail,
+    headline,
+    content,
+  },
+}) {
   return (
     <Layout>
       <Head>
@@ -31,27 +49,27 @@ export default function Detail({ single }) {
       <Container>
         <div className="md:w-6/12 w-full mx-auto flex items-center flex-col">
           <PostMetaTitle
-            slug="aaaaaa"
-            category="UI Design"
-            date="July 2, 2021"
-            title="Understanding color theory: the color wheel and finding complementary colors"
+            slug={slug}
+            category={category.name}
+            date={formatDate(published_at)}
+            title={title}
             center
           />
           <PostAuthor
-            authorName="Leslie Alexander"
-            authorJob="UI Designer"
-            authorAvatar="/author-1.png"
+            authorName={author.name}
+            authorJob={author.job}
+            authorAvatar={process.env.NEXT_PUBLIC_APIURL + author.avatar.url}
           />
         </div>
         <div className="md:w-10/12 w-full mx-auto my-10">
-          <img src="/detail-image.png" className="w-full rounded-lg" />
+          <img
+            src={process.env.NEXT_PUBLIC_APIURL + thumbnail.formats.large.url}
+            className="w-full rounded-lg"
+          />
         </div>
         <div className="md:w-8/12 w-full mx-auto leading-relaxed">
-          <p className="text-xl mb-4">
-            Male sixth sea it a. Brought was signs female darkness signs form cattle land grass whose from subdue also they're their brought seas isn't, to day from bearing grass third midst after beginning man which you're. Dry, gathering beginning given made them evening.
-          </p>
-          <p className="mb-4">Lights dry. Thing, likeness, forth shall replenish upon abundantly our green. Seed green sea that lesser divided creature beginning land him signs stars give firmament gathered. Wherein there their morning a he grass. Don't made moving for them bring creature us you'll tree second deep good unto good may. Us yielding.</p>
-          <p>Have. Man upon set multiply moved from under seasons abundantly earth brought a. They're open moved years saw isn't morning darkness. Over, waters, every let wherein great were fifth saw was lights very our place won't and him Third fourth moving him whales behold. Beast second stars lights great was don't green give subdue his. Third given made created, they're forth god replenish have whales first can't light was. Herb you'll them beast kind divided. Were beginning fly air multiply god Yielding sea don't were forth.</p>
+          <p className="text-xl mb-4">{headline}</p>
+          <ReactMarkdown className="mb-4">{content}</ReactMarkdown>
         </div>
       </Container>
     </Layout>
